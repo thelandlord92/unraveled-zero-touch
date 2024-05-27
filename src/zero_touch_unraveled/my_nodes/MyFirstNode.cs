cb4d56b6479ca004556d54db543c40ff285c2c09
thelandlord92 <bayo@onile.ai>
2024-05-27 15:07:15 +0200

    Added comments and XML doc file.

--- src/zero_touch_unraveled/my_nodes/MyFirstNode.cs
+++ src/zero_touch_unraveled/my_nodes/MyFirstNode.cs
@@ -8,9 +8,11 @@ namespace zero_touch_unraveled
 {
     public class MyFirstNode
     {
+        // this hides the overall class as a node.
         private MyFirstNode() { }
         public static string HelloWorld()
         {
+            // returns one output of hello world.
             return "Hello world " + DateTime.Now;
         }
     }
