c1e5b6c027012f35b68c7f424b3cedcdbdb5875a
thelandlord92 <bayo@onile.ai>
2024-05-27 15:44:12 +0200

    Added comments to first node.

--- src/zero_touch_unraveled/my_nodes/MyFirstNode.cs
+++ src/zero_touch_unraveled/my_nodes/MyFirstNode.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace zero_touch_unraveled
 {
+    /// <summary>
+    /// Wrapper class for my first node.
+    /// </summary>
     public class MyFirstNode
     {
         // this hides the overall class as a node.
         private MyFirstNode() { }
+
+        /// <summary>
+        /// My first node that ouputs a string that says Hello World.
+        /// </summary>
+        /// <returns></returns>
         public static string HelloWorld()
         {
             // returns one output of hello world.
