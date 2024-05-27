c676f841bc1bf24cfdb92dde96205fe3db4ba358
thelandlord92 <bayo@onile.ai>
2024-05-27 22:52:51 +0200

    Add Nuget references to Dynamo libraries.

---
+++ src/zero_touch_unraveled/packages/DynamoVisualProgramming.Revit.2.18.1.8944/tools/install.ps1
@@ -0,0 +1,33 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$asm_root_folder_name = [System.IO.Path]::Combine($installPath,`
+"lib");
+
+$net_folders = [System.IO.Directory]::GetDirectories(`
+$asm_root_folder_name, 'net*', 'TopDirectoryOnly');
+
+$file_names = New-Object `
+'System.Collections.Generic.HashSet[string]';
+
+foreach ($net in $net_folders) {
+
+    $files = [System.IO.Directory]::EnumerateFiles($net,"*.dll"`
+    ,"AllDirectories");
+
+        foreach ($file in $files) {
+
+            $file_name = [System.IO.Path]::`
+            GetFileNameWithoutExtension($file);
+
+            $file_names.Add($file_name);
+    }
+}
+
+foreach ($reference in $project.Object.References) {
+
+    if($file_names.Contains($reference.Name)) {
+
+        $reference.CopyLocal = $false;
+        $reference.SpecificVersion = $false;
+    }
+}
